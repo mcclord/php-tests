59143c773ee0d85bf4bbdd72911c8654c67a1c9d
mcclord <101862398+mcclord@users.noreply.github.com>
2022-03-18 17:34:35 +0800

    Added view and CSS
    Added view.php and
    
    Added CSS styles.css

--- classes/Exporter.php
+++ classes/Exporter.php
@@ -4,6 +4,7 @@ use Illuminate\Support;
 use LSS\Array2Xml;
 
 require 'Player.php';
+require 'View.php';
 
 // retrieves & formats data from the database for export
 class Exporter {
@@ -127,29 +128,11 @@ class Exporter {
 
     // wrap html in a standard template
     public function htmlTemplate($html) {
-        return '
-<html>
-<head>
-<style type="text/css">
-    body {
-        font: 16px Roboto, Arial, Helvetica, Sans-serif;
-    }
-    td, th {
-        padding: 4px 8px;
-    }
-    th {
-        background: #eee;
-        font-weight: 500;
-    }
-    tr:nth-child(odd) {
-        background: #f4f4f4;
-    }
-</style>
-</head>
-<body>
-    ' . $html . '
-</body>
-</html>';
+		
+		 $View = new View();
+		 return $View->getHtml($html);
+		 
+         
     }
 }
 
--- classes/View.php
+++ classes/View.php
@@ -1,12 +1,19 @@
 <?php
-use Illuminate\Support;  // https://laravel.com/docs/5.8/collections - provides the collect methods & collections class
-use LSS\Array2Xml;
-require_once('classes/Exporter.php');
 
 class View {
 
-    public function __construct($args) {
-        $this->args = $args;
+    function getHtml($html) {
+
+       return '
+			<html>
+			<head>
+				<link type="text/css" rel="stylesheet" href="css/styles.css">
+			</head>
+			<body>
+				' . $html . '
+			</body>
+			</html>';
+
     }
 
 
